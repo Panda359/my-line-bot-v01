52b5d470429591653686ef77e87be17ff70c6eb8
Panda359 <jiewjaw@gmail.com>
2020-02-13 16:58:50 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,10 +7,6 @@ from linepy import *
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('fSTr2G4/t3EQ2vocVfa7xSRDUWDJVkKtPYFOTfQOhSE+H4XS9iir2Scv/jjqNAl4OC3fZlgTSHPvEAv0SC49WkhLza2J3qWAWJ5BotlBD1brgoCWAjJT4KGZDNeABUBMHgcn68Ofb+t7srQviEYjOgdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('91aba13ad024feeb462ae8ae2a72a7ba')
-
-
 cl = LINE("fSTr2G4/t3EQ2vocVfa7xSRDUWDJVkKtPYFOTfQOhSE+H4XS9iir2Scv/jjqNAl4OC3fZlgTSHPvEAv0SC49WkhLza2J3qWAWJ5BotlBD1brgoCWAjJT4KGZDNeABUBMHgcn68Ofb+t7srQviEYjOgdB04t89/1O/w1cDnyilFU=")
 #cl = LINE("TOKENMU")
 #cl = LINE("Email","Password")
@@ -18,6 +14,12 @@ cl = LINE("fSTr2G4/t3EQ2vocVfa7xSRDUWDJVkKtPYFOTfQOhSE+H4XS9iir2Scv/jjqNAl4OC3fZ
 cl.log("Auth Token : " + str(cl.authToken))
 channelToken = cl.getChannelResult()
 cl.log("Channel Token : " + str(channelToken))
+
+line_bot_api = LineBotApi('fSTr2G4/t3EQ2vocVfa7xSRDUWDJVkKtPYFOTfQOhSE+H4XS9iir2Scv/jjqNAl4OC3fZlgTSHPvEAv0SC49WkhLza2J3qWAWJ5BotlBD1brgoCWAjJT4KGZDNeABUBMHgcn68Ofb+t7srQviEYjOgdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('91aba13ad024feeb462ae8ae2a72a7ba')
+
+
+
 
 
 @app.route("/")
